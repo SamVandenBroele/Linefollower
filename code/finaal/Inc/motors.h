f12f657789a8dd9a5a5e85219d31e2ae3ab9eb86
SamVandenBroele <sam.vandenbroele@student.hogent.be>
2026-08-30 22:19:00 +0200

    Update motors.h

--- code/finaal/Inc/motors.h
+++ code/finaal/Inc/motors.h
@@ -1,1 +1,34 @@
+#ifndef MOTORS_H_
+#define MOTORS_H_
+
+#include <stdint.h>
+
+/* =========================
+   MOTOR FUNCTIES
+   ========================= */
+
+/* Initialiseer TB6612FNG en PWM */
+void Motors_Init(void);
+
+/* Motor A snelheid
+   speed: -1000 ... +1000
+   negatief = achteruit
+   positief = vooruit
+   0 = stop
+*/
+void MotorA_SetSpeed(int16_t speed);
+
+/* Motor B snelheid */
+void MotorB_SetSpeed(int16_t speed);
+
+/* Stop beide motoren */
+void Motors_Stop(void);
+
+/* Zet TB6612 in standby */
+void Motors_Standby(void);
+
+/* Activeer TB6612 */
+void Motors_Enable(void);
+
+#endif /* MOTORS_H_ */
 
